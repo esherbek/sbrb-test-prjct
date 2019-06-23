156c58f7f213056b0344f2db35ee533a0aa9e767
Esher bek <esherbek@github.com>
2019-06-23 23:40:58 +0400

    23062019

--- src/app/journal/dashboard/dashboard.component.ts
+++ src/app/journal/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataserviceService } from '../dataservice.service';
+import { MOCKDATA } from 'src/app/mockdata';
+import { Register } from 'src/app/register';
+import { Postage } from 'src/app/postage';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,14 +10,22 @@ import { DataserviceService } from '../dataservice.service';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
-
+  public registers: Register[];
+  public postages: postages[];
   constructor(
     // Сервис для обращения к базе данных
     private service: DataserviceService
   ) { }
 
+  // postages:  any =  MOCKDATA.getPostages;
+  // registers: Register[] = MOCKDATA.getRegisters;
+  
   ngOnInit() {
+    MOCKDATA.getRegisters().subscribe(reg => {
+      this.registers = reg;
+    });
     
+  
   }
 
 
